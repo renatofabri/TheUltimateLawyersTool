576d17732f8d1f962a794a0b179199d6c3f7754f
razr <razr74@users.noreply.github.com>
2014-10-25 14:51:41 -0200

    View changes

--- Common/Methods.py
+++ Common/Methods.py
@@ -7,19 +7,21 @@ Created on Oct 19, 2014
 class Masks:
 
     def __init__(self):
-        print 'lol'
+        pass
 
 
     def convert_number_to_process_mask(self, num_processo):
-
+        '''
+        TODO: add method description
+        '''
         pos = [7,10,15,17,20]
 
         if len(num_processo) == 20:
-            hash = num_processo
-            hashlist = list(hash)
+            hash_number = num_processo
+            hashlist = list(hash_number)
 
             for n in pos:
                 hashlist.insert(n, '.')
 
-            hash = ''.join(hashlist)
-            return hash
+            hash_number = ''.join(hashlist)
+            return hash_number
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -7,15 +7,16 @@ from django.shortcuts import render_to_response
 from Common.Templates import DASHBOARD_HOME
 from TheUltimateLawyersTool.settings import PAGE_TITLE
 from django.template.context import RequestContext
+from CourtService.TJSP import TJSP
 
 VIEW_NAME = 'Dashboard'
 
 def home(request):
 
-    data = {'title': PAGE_TITLE % VIEW_NAME,}
+    tjsp = TJSP()
+    complete_page = tjsp.getLawsuitById('2014', '1')
+
+    data = {'title': PAGE_TITLE % VIEW_NAME,
+            'content': complete_page.text}
 
     return render_to_response(DASHBOARD_HOME, data, context_instance=RequestContext(request))
-
-
-if __name__ == '__main__':
-    print "lol"
